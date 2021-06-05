e1874c35ad0a661007f2fe69bb6256793bed5fda
Alejandro <wds@disnot.com>
2021-06-05 19:52:37 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,20 +1,25 @@
-const request = require('request');
-const http = require("http");
+const request = require('request')
 
-let server = http.createServer((req,res)=>{
-    const start =  0
-    const count =  8
-    var options = {
-      method: 'get',
-      url: `http://cn.bing.com/HPImageArchive.aspx?format=js&idx=${start}&n=${count}`
-    };
-    request(options, function(error, response) {
-      console.log(response)
-      res.write(response)
-    })
-    res.statusCode = 200;
-    res.setHeader("content-type","text/plain")
-    res.end('hahahah')
+module.exports = async(req, res) => {
+  const start = req.query.start ? req.query.start : 0
+  const count = req.query.count ? req.query.count : 8
+  
+  const options = {
+    method: 'get',
+    url: `http://cn.bing.com/HPImageArchive.aspx?format=js&idx=${start}&n=${count}`
+  }
+  
+  request(options, function(error, response) {
+    if (error) { // if error fetching time
+      res.json({
+        status: false,
+        msg: "Failed to get time data"
+      })
+    } else {
+      res.json({
+        status: 'ok',
+        data: JSON.parse(response.body)
+      })
+    }
+  })
 })
-
-server.listen(3000)
