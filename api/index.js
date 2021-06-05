e59ee0a1b0bb6a000b1d27115b37458decef5666
Alejandro <wds@disnot.com>
2021-06-05 20:10:06 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,25 +1,6 @@
-const request = require('request')
-
 module.exports = async(req, res) => {
-  const start = req.query.start ? req.query.start : 0
-  const count = req.query.count ? req.query.count : 8
-  
-  var options = {
-    method: 'get',
-    url: `http://cn.bing.com/HPImageArchive.aspx?format=js&idx=${start}&n=${count}`
-  }
-  
-  request(options, function(error, response) {
-    if (error) { // if error fetching time
-      res.json({
-        status: false,
-        msg: "Failed to get time data"
-      })
-    } else {
-      res.json({
-        status: 'ok',
-        data: response.body
-      })
-    }
+  res.json({
+    status: 'ok',
+    msg: "hello, vercel api"
   })
 })
